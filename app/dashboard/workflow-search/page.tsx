1dfa204f997e0a094bedcd1c11968b6c9b068d6c
Théo Dufort <theodufort05@gmail.com>
2025-01-28 04:01:43 -0500

    feat: Convert workflow search page to client-side component with user authentication

--- app/dashboard/workflow-search/page.tsx
+++ app/dashboard/workflow-search/page.tsx
@@ -1,7 +1,28 @@
+"use client";
 import WorkflowSearch from "@/components/workflow-search";
+import { getUser } from "@/utils/queries";
+import { createClient } from "@/utils/supabase/client";
 import { getWorkflows } from "@/utils/supabase/workflows";
+import { User } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
 
-export default async function WorkflowSearchPage() {
+export default function WorkflowSearchPage() {
+  const supabase = createClient();
   const workflows = await getWorkflows();
-  return <WorkflowSearch items={workflows} />;
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const getUserCall = async () => {
+      const user = await getUser(supabase);
+      if (user) {
+        setUser(user);
+      } else {
+        console.log("User not authenticated");
+      }
+    };
+    getUserCall();
+  }, [supabase]);
+
+  if (!user) return <div></div>;
+
+  return <WorkflowSearch items={workflows} user_id={user.id} />;
 }
