80eedf3fdd63ea65e52c07d5f4ec84aa9998a5f1
Théo Dufort <theodufort05@gmail.com>
2025-02-09 18:22:46 -0500

    remove next logo

--- components/header.tsx
+++ components/header.tsx
@@ -1,6 +1,5 @@
 "use client";
 import Link from "next/link";
-import NextLogo from "./next-logo";
 import { Button } from "./ui/button";
 import { useRouter } from "next/navigation";
 import OpenForgeLogo from "./openforge-logo";
