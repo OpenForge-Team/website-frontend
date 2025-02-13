dec98ebbf3688402a7cdb3ba5909353ad602d4c8
Théo Dufort <theodufort05@gmail.com>
2025-02-13 16:41:13 -0500

    fix: Update API chat query route with type and streaming response

--- app/api/(public)/docs/chat/query/route.ts
+++ app/api/(public)/docs/chat/query/route.ts
@@ -1,6 +1,7 @@
 import { validateApiKey } from "@/utils/public-api/validate-query";
 import { AskAIChat } from "@/utils/supabase/rag/chat/supabase-hybrid/ask";
 import { type NextRequest } from "next/server";
+import { ApiChatQueryResponse } from "../types";
 export const revalidate = 30;
 
 export async function GET(request: NextRequest) {
@@ -17,19 +18,17 @@ export async function GET(request: NextRequest) {
       const subject_id = searchParams.get("subject_id") || undefined;
 
       if (!message) {
-        return new Response("Message is required", { status: 400 });
+        return new Response({}<ApiChatQueryResponse>, { status: 400 });
       }
 
       await AskAIChat({
         user_id: userIdForKey,
-        workspace_id: "public", // Using "public" for API requests
+        workspace_id: "", // Using "public" for API requests
         message,
         is_from_widget: false,
-        stream: false,
+        stream: true,
         subject_id,
       });
-
-      return new Response("Success", { status: 200 });
     }
   }
 }
