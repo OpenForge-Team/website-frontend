533e1e4beaf2cdf101c0667051e41aa978544b9e
Théo Dufort <theodufort05@gmail.com>
2025-02-13 16:27:20 -0500

    feat: add public chat query API route

---
+++ app/api/(public)/docs/chat/query/route.ts
@@ -0,0 +1,25 @@
+import { validateApiKey } from "@/utils/public-api/validate-query";
+import { AskAIChat } from "@/utils/supabase/rag/chat/supabase-hybrid/ask";
+import { type NextRequest } from "next/server";
+export const revalidate = 30;
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+
+  //api key validation
+  const api_key = searchParams.get("api_key");
+
+  if (api_key) {
+    const userIdForKey = await validateApiKey(api_key);
+    if (userIdForKey) {
+      //params
+      const message = searchParams.get("message");
+      const subject_id = searchParams.get("subject_id");
+
+      await AskAIChat({
+        user_id: userIdForKey,
+        subject_id: subject_id,
+      });
+    }
+  }
+}
