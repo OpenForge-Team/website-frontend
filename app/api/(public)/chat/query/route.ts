8e83f023e635e78967b59af03d6fbd29ce01021f
Théo Dufort <theodufort05@gmail.com>
2025-02-14 08:47:55 -0500

    feat: add public chat query route endpoint

---
+++ app/api/(public)/chat/query/route.ts
@@ -0,0 +1,76 @@
+import { validateApiKey } from "@/utils/public-api/validate-query";
+import { AskAIChat } from "@/utils/supabase/rag/chat/supabase-hybrid/ask";
+import { type NextRequest } from "next/server";
+import { ApiChatQueryResponse } from "../types";
+import { ApiErrorResponse } from "../../types";
+export const revalidate = 30;
+
+/**
+ * @swagger
+ * /api/chat/query:
+ *   get:
+ *     description: Returns a chat response
+ *     responses:
+ *       200:
+ *         description: Chat response from the AI
+ *       400:
+ *         description: Multiple errors possible, please check error message
+ *       401:
+ *         description: Api key passed is not valid
+ */
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+
+  //api key validation
+  const api_key = searchParams.get("api_key");
+
+  if (api_key) {
+    const userIdForKey = await validateApiKey(api_key);
+    if (userIdForKey) {
+      //params
+      const message = searchParams.get("message");
+      const subject_id = searchParams.get("subject_id") || undefined;
+      const stream = searchParams.get("subject_id");
+
+      if (!message) {
+        const errorResponse: ApiErrorResponse = {
+          error: "Message is required",
+          status: 400,
+        };
+        return new Response(JSON.stringify(errorResponse), {
+          status: 400,
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+
+      const response = await AskAIChat({
+        user_id: userIdForKey,
+        workspace_id: "public", // Using "public" for API requests
+        message,
+        is_from_widget: false,
+        stream: true,
+        subject_id,
+      });
+      if (stream === "true") {
+        return new Response(response, {
+          status: 200,
+          headers: { "Content-Type": "text/plain" },
+        });
+      } else {
+        return new Response(response, {
+          status: 200,
+          headers: { "Content-Type": "application/json" },
+        });
+      }
+    }
+  } else {
+    const unauthorizedResponse: ApiErrorResponse = {
+      error: "Invalid API key",
+      status: 401,
+    };
+    return new Response(JSON.stringify(unauthorizedResponse), {
+      status: 401,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
+}
