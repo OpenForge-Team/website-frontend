416431a5bbfb8c3d864cbae5ed99aa6f5e4f80fb
Théo Dufort <theodufort05@gmail.com>
2025-02-14 19:46:51 -0500

    refactor: Update ApiUsageChart to accept dynamic chart data and simplify rendering

--- components/api-usage-chart.tsx
+++ components/api-usage-chart.tsx
@@ -13,32 +13,29 @@ const chartConfig = {
   },
   mobile: {
     label: "Mobile",
-    color: "#60a5fa",
+    color: "#",
   },
 } satisfies ChartConfig;
-const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
-];
-export default function ApiUsageChart() {
+interface props {
+  chartData: [];
+}
+export default function ApiUsageChart({ chartData }: props) {
+  console.log(chartData);
   return (
     <ChartContainer config={chartConfig} className="h-[200px] w-full">
       <BarChart accessibilityLayer data={chartData}>
         <CartesianGrid vertical={false} />
         <XAxis
-          dataKey="month"
+          color="black"
+          dataKey="day"
+          label={"Date"}
           tickLine={false}
-          tickMargin={10}
+          tickMargin={30}
           axisLine={false}
-          tickFormatter={(value) => value.slice(0, 3)}
+          tickFormatter={(value) => value.slice(0, 2)}
         />
         <ChartTooltip content={<ChartTooltipContent />} />
-        <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-        <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+        <Bar dataKey="messages" fill="var(--color-desktop)" radius={4} />
       </BarChart>
     </ChartContainer>
   );
