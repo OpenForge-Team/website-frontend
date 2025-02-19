ea24c548814315870a508b61d2d0ecc9facf5213
Théo Dufort <theodufort05@gmail.com>
2025-02-19 11:11:04 -0500

    analytics

--- app/dashboard/integration-analytics/page.tsx
+++ app/dashboard/integration-analytics/page.tsx
@@ -1,3 +1,25 @@
+"use client";
+import { parseQueries } from "@/utils/ai/analytics/find-subjects";
+import { getUser } from "@/utils/queries";
+import { createClient } from "@/utils/supabase/client";
+import { User } from "@supabase/supabase-js";
+import { useEffect, useState } from "react";
+
 export default function IntegrationAnalyticsPage() {
+  const supabase = createClient();
+  const [user, setUser] = useState<User | null>(null);
+  useEffect(() => {
+    const getUserCall = async () => {
+      const user = await getUser(supabase);
+      if (user) {
+        setUser(user);
+        await parseQueries(user.id);
+      } else {
+        console.log("User not authenticated");
+      }
+    };
+    getUserCall();
+  }, [supabase]);
+
   return <div></div>;
 }
--- utils/ai/analytics/find-subjects.ts
+++ utils/ai/analytics/find-subjects.ts
@@ -1,3 +1,55 @@
 "use server";
 
-export async function parseQueries() {}
+import { createClient } from "@/utils/supabase/server";
+
+export async function parseQueries(user_id: string) {
+  const supabase = await createClient();
+  const { data, error } = await supabase
+    .from("api_queries_message_analytics")
+    .select(
+      `
+    api_queries_message_subject_id,
+    api_queries!inner (
+      data->>message,
+      api_key!inner (
+        user_id
+      )
+    ),
+    api_queries_message_subjects!inner (
+      id,
+      name
+    )
+  `
+    )
+    .eq("api_queries.api_key.user_id", user_id)
+    .not("api_queries_message_subject_id", "is", null);
+
+  console.log(data);
+  if (error) {
+    console.error("Error fetching data:", error);
+    return [];
+  }
+
+  // Process data to group messages by subject_id
+  const messageBySubject = data?.reduce((acc, x) => {
+    // Create the subject object if it doesn't exist
+    if (!acc[x.api_queries_message_subject_id]) {
+      acc[x.api_queries_message_subject_id] = {
+        subject_id: x.api_queries_message_subject_id,
+        subject_name: x.api_queries_message_subjects.name,
+        messages: [],
+      };
+    }
+
+    // Add the message to the respective subject's messages array
+    acc[x.api_queries_message_subject_id].messages.push(x.api_queries.message);
+
+    return acc;
+  }, {});
+
+  // Convert the grouped data into an array
+  const result = Object.values(messageBySubject);
+
+  console.log(result);
+  return result;
+}
